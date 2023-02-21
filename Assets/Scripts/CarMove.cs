38852215725fa4930573b17dee445b54d2a84f30
Amenisk <k.ziyatdinov2003@gmail.com>
2023-02-21 23:04:50 +0300

    Created project

---
+++ Assets/Scripts/CarMove.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMove : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(transform.position.x > -2.77f)
+            {
+                var target = new Vector3(transform.position.x - 1.5f, transform.position.y, transform.position.z);
+                transform.position = Vector3.MoveTowards(transform.position, target, _movementSpeed);
+            }       
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (transform.position.x < 3.23f)
+            {
+                var target = new Vector3(transform.position.x + 1.5f, transform.position.y, transform.position.z);
+                transform.position = Vector3.MoveTowards(transform.position, target, _movementSpeed);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SecondRoadMove.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class SecondRoadMove : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var target = new Vector3(transform.position.x, transform.position.y - 0.05f, transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, target, _movementSpeed);
+        if(transform.localPosition.y <= -22.93f)
+        {
+            transform.position = new Vector3(transform.position.x, 21.05f, transform.position.z);
+        }
+    }
+}
